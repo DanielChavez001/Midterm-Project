39995bccc7261a61a61ab5c6bd396ab00a7376b9
DanielChavez001 <76968877+DanielChavez001@users.noreply.github.com>
2021-03-11 12:18:37 -0800

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,76 @@
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+// geolocation
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Geolocation Error</p>";
+}
+// my position
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    
+    getWeather(latitude, longitude);
+}
+// error
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+// celsius weather
+const weather = {};
+weather.temperature = {
+    unit : "celsius"
+}
+// fetch and api key
+const key = "0c78fdcabc05dc34ed0b9236df299b71";
+function getWeather(latitude, longitude){
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+//convert kelvin to celsius
+const KELVIN = 273;
+// connect to the ui
+function displayWeather(){
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0c78fdcabc05dc34ed0b9236df299b71
